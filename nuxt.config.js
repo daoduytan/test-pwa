888c79d5c4eb99d92ac55fad2af106bc51473699
daoduytan <ddtan2212@gmail.com>
2022-05-30 13:36:03 +0700

    update pwa

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,44 +1,49 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
+  target: "static",
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'test-pwa',
+    title: "test-pwa",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en",
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-  ],
+  buildModules: ["@nuxtjs/pwa"],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
+  modules: [],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
-}
+  build: {},
+  pwa: {
+    manifest: {
+      name: "GCO Software",
+      short_name: "GCO Software",
+      lang: "en",
+      theme_color: "#ffffff",
+    },
+    icon: {
+      fileName: "icon.png",
+      sizes: [64, 120, 144, 152, 192, 384, 512],
+    },
+  },
+};
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,35 @@
 <template>
-  <Tutorial />
+  <div>
+    <Tutorial />
+
+    <button @click="thisClick">s</button>
+  </div>
 </template>
 
 <script>
 export default {
   name: "1IndexPage",
+
+  data: {
+    test: null,
+  },
+
+  methods: {
+    handleScroll(e) {
+      // Your scroll handling here
+      console.log({ e });
+
+      this.test = e;
+    },
+    thisClick() {
+      console.log("dsala", this.test);
+    },
+  },
+  beforeMount() {
+    window.addEventListener("beforeinstallprompt", this.handleScroll);
+  },
+  beforeDestroy() {
+    window.removeEventListener("beforeinstallprompt", this.handleScroll);
+  },
 };
 </script>
